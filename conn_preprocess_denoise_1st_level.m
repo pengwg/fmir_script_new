3074be7811bfe9e34aed47766b2a71562b84b09e
Peng Wang <pengwg@gmail.com>
2024-05-21 14:39:24 -0400

    Add setup only option

--- conn_preprocess_denoise_1st_level.m
+++ conn_preprocess_denoise_1st_level.m
@@ -1,15 +1,21 @@
 clear
 
+% Load un-preprocessed data into gui only
+setup_only = false;
+
 % Has to be absolute path. Relative path not working
 path_subjects = '/home/pw0032/Work/fusOUD/fmri';
 
-% Use 'sub-*' for all subjects
+% Single subject
 subjects_matching = 'sub-221-FUS'; 
+
+% For all subjects
+% subjects_matching = 'sub-*';
 
 BATCHFILENAME = [path_subjects '/fusOUD_90Days'];
 conditions = {'RS_Baseline', 'RS_7Days', 'RS_30Days', 'RS_90Days'}; 
 % conditions = {'RS_Baseline', 'RS_7Days'};
-% batch.parallel.N = 2;   
+
 
 if isfolder ([path_subjects filesep subjects_matching])
     sub_names = {subjects_matching};
@@ -25,12 +31,6 @@ end
 % MR parameters
 TR = 1;
 
-batch.filename = BATCHFILENAME;
-batch.Setup.isnew = 1;
-batch.Setup.nsubjects = length(sub_names);
-batch.Setup.RT = TR;
-batch.Setup.conditions.names = conditions;
-
 for n = 1 : length(sub_names)
     sessions = dir([path_subjects, filesep, sub_names{n}, filesep, 'ses*']);
 
@@ -38,9 +38,11 @@ for n = 1 : length(sub_names)
         ses_name = sessions(m).name;
         func_path = [path_subjects filesep sub_names{n} filesep ses_name filesep 'func'];
         anat_path = [path_subjects filesep sub_names{n} filesep ses_name filesep 'anat'];
+        fmap_path = [path_subjects filesep sub_names{n} filesep ses_name filesep 'fmap'];
 
         RS_file = dir([func_path filesep sub_names{n} '_' ses_name '_task-RS_bold.nii']);
         anat_file = dir([anat_path filesep sub_names{n} '_' ses_name '_T1w.nii']);
+        fmap_file = dir([fmap_path filesep sub_names{n} '_' ses_name '_T1w.nii']);
 
         if isempty(RS_file)
             warning([func_path ': functional file not found!'])
@@ -69,32 +71,55 @@ for n = 1 : length(sub_names)
     
 end
 
-% Default options (uses all ROIs in conn/rois/ directory); see conn_batch for additional options
-% (realignment/coregistration/segmentation/normalization/smoothing)
-
-batch.Setup.preprocessing.steps = 'default_mni';
-batch.Setup.preprocessing.sliceorder = 'interleaved (Siemens)';
-batch.Setup.done = 1;
-batch.Setup.overwrite = 'Yes';
+batch.filename = BATCHFILENAME;
+batch.Setup.isnew = 1;
+batch.Setup.nsubjects = length(sub_names);
+batch.Setup.RT = TR;
+batch.Setup.conditions.names = conditions;
 
 [conn_path, ~, ~] = fileparts(which('conn'));
-
 batch.Setup.rois.files{1} = [conn_path '/rois/atlas.nii'];
 batch.Setup.rois.files{2} = [conn_path '/rois/networks.nii'];
 % batch.Setup.rois.files{1} = 'ROIs/AndyROIs.nii';
 % batch.Setup.rois.multiplelabels = 1;
 
-% CONN Denoising                                    % Default options (uses White Matter+CSF+realignment+scrubbing+conditions as confound regressors); see conn_batch for additional options
+if setup_only
+    conn_batch(batch);
+    conn
+    conn('load', BATCHFILENAME);
+    conn gui_setup
+    return
+end
+
+% Preprocessing using 'VDM' and 'default_mni' options
+batch.Setup.preprocessing.steps = {'functional_label_as_original'; 
+                                   % 'functional_vdm_create'; 
+                                   % 'functional_vdm_apply'; 
+                                   'functional_realign&unwarp';
+                                   'functional_center';
+                                   'functional_slicetime';
+                                   'functional_art';
+                                   'functional_segment&normalize_direct';
+                                   'functional_label_as_mnispace';
+                                   'structural_center';
+                                   'structural_segment';
+                                   'functional_smooth';
+                                   'functional_label_as_smoothed'};
+
+batch.Setup.preprocessing.sliceorder = 'interleaved (Siemens)';
+batch.Setup.done = 1;
+batch.Setup.overwrite = 'Yes';
+
+% Denoising                                    % Default options (uses White Matter+CSF+realignment+scrubbing+conditions as confound regressors); see conn_batch for additional options
 batch.Denoising.filter = [0.01, 0.1];                 % frequency filter (band-pass values, in Hz)
 batch.Denoising.done = 1;
 batch.Denoising.overwrite = 'Yes';
 
-% CONN Analysis                                     % Default options (uses all ROIs in conn/rois/ as connectivity sources); see conn_batch for additional options
+% Analysis                                     % Default options (uses all ROIs in conn/rois/ as connectivity sources); see conn_batch for additional options
 batch.Analysis.done = 1;
 batch.Analysis.overwrite = 'Yes';
 
 conn_batch(batch);
-
 conn
 conn('load', BATCHFILENAME);
 conn gui_analyses
