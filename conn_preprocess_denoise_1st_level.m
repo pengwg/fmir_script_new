885f1fcf789f73cb86e5db66b6e24009e4942613
Peng Wang <pengwg@gmail.com>
2024-05-24 15:35:12 -0400

    Use subjects and sessions list

--- conn_preprocess_denoise_1st_level.m
+++ conn_preprocess_denoise_1st_level.m
@@ -1,73 +1,74 @@
 clear
 
 % Load un-preprocessed data into gui only
-setup_only = false;
+setup_only = true;
 
 % Has to be absolute path. Relative path not working
 path_subjects = '/home/peng/Work/fusOUD/fmri';
-
-% Single subject
-subjects_matching = 'sub-221-FUS'; 
-
-% For all subjects
-% subjects_matching = 'sub-*';
+subjects_list = {'sub-221-FUS', 'sub-222-FUS', 'sub-223-FUS'};
+sessions_list = {'ses-00', 'ses-07', 'ses-30', 'ses-90'};
 
 BATCHFILENAME = [path_subjects '/fusOUD_90Days'];
-conditions = {'RS_Baseline', 'RS_7Days', 'RS_30Days', 'RS_90Days'}; 
-% conditions = {'RS_Baseline', 'RS_7Days'};
 
-
-if isfolder ([path_subjects filesep subjects_matching])
-    sub_names = {subjects_matching};
-else
-    subjects = dir([path_subjects filesep subjects_matching]);
-    sub_names = {subjects.name};
-end
-
-if isempty(sub_names)
-    error('No subjects found!')
-end
 
 % MR parameters
 TR = 1;
 blipdir = -1;
 
-for n = 1 : length(sub_names)
-    sessions = dir([path_subjects, filesep, sub_names{n}, filesep, 'ses*']);
+sub_count = 0;
+for n = 1 : length(subjects_list)
+    sub_name = subjects_list{n};
+    if ~isfolder([path_subjects filesep sub_name])
+        warning([sub_name ': does not exist!'])
+        continue
+    end
 
-    for m = 1 : length(sessions)
-        ses_name = sessions(m).name;
-        func_path = [path_subjects filesep sub_names{n} filesep ses_name filesep 'func'];
-        anat_path = [path_subjects filesep sub_names{n} filesep ses_name filesep 'anat'];
-        fmap_path = [path_subjects filesep sub_names{n} filesep ses_name filesep 'fmap'];
+    ses_count = 0;
+    for m = 1 : length(sessions_list)
+        ses_name = sessions_list{m};
 
-        RS_file = dir([func_path filesep sub_names{n} '_' ses_name '_task-RS_bold.nii']);
-        anat_file = dir([anat_path filesep sub_names{n} '_' ses_name '_T1w.nii']);
-        fmap_file = dir([fmap_path filesep sub_names{n} '_' ses_name '*.nii']);
+        if ~isfolder([path_subjects filesep sub_name filesep ses_name])
+            fprintf(['**** ' sub_name ' ' ses_name ' **** does not exist!\n\n'])
+            continue
+        end
+
+        func_path = [path_subjects filesep sub_name filesep ses_name filesep 'func'];
+        anat_path = [path_subjects filesep sub_name filesep ses_name filesep 'anat'];
+        fmap_path = [path_subjects filesep sub_name filesep ses_name filesep 'fmap'];
+
+        RS_file = dir([func_path filesep sub_name '_' ses_name '_task-RS_bold.nii']);
+        anat_file = dir([anat_path filesep sub_name '_' ses_name '_T1w.nii']);
+        
+        mag_files = dir([fmap_path filesep sub_name '_' ses_name '*magnitude*.nii']);
+        pd_file = dir([fmap_path filesep sub_name '_' ses_name '*phasediff.nii']);
+        fmap_files = [mag_files; pd_file];
 
         if isempty(RS_file)
-            warning([func_path ': functional file not found!'])
+            warning([func_path ': functional file not found, skip this session!'])
             continue
         end
 
         if isempty(anat_file)
-            warning([anat_path ': structural file not found!'])
+            warning([anat_path ': structural file not found, skip this session!'])
             continue
         end
         
-        disp(['**** ' sub_names{n} ' - ' ses_name ' ****'])
+        disp(['**** ' sub_name ' ' ses_name ' ****'])
         disp(['Structural: ' anat_file(1).name])
         disp(['Functional: ' RS_file(1).name])
-        disp([repmat('FieldMap: ', length({fmap_file.name}), 1) char({fmap_file.name})])
+        disp([repmat('FieldMap: ', length({fmap_files.name}), 1) char({fmap_files.name})])
         fprintf('\n')
 
-        if length(fmap_file) ~= 3
-            warning([fmap_path ': magnitude1 + magnitude2 + phasediff requried!'])
+        if length(fmap_files) ~= 3
+            warning([fmap_path ': magnitude1 + magnitude2 + phasediff not found, skip this session!'])
             continue
         end
 
-        batch.Setup.functionals{n}{m} = [func_path filesep RS_file(1).name];
-        batch.Setup.structurals{n}{m} = [anat_path filesep anat_file(1).name];
+        % This is a valid session now
+        ses_count = ses_count + 1;
+
+        batch.Setup.functionals{sub_count+1}{ses_count} = [func_path filesep RS_file(1).name];
+        batch.Setup.structurals{sub_count+1}{ses_count} = [anat_path filesep anat_file(1).name];
 
         % Default secondary data after preprocess
         batch.Setup.secondarydatasets(1).functionals_type = 2;
@@ -75,26 +76,31 @@ for n = 1 : length(sub_names)
         % Load field map
         batch.Setup.secondarydatasets(2).functionals_label = 'fmap';
         batch.Setup.secondarydatasets(2).functionals_type = 4;
-        batch.Setup.secondarydatasets(2).functionals_explicit{n}{m} = [repmat([fmap_path filesep], 3, 1) char({fmap_file.name})];
+        batch.Setup.secondarydatasets(2).functionals_explicit{sub_count+1}{ses_count} = [repmat([fmap_path filesep], 3, 1) char({fmap_files.name})];
 
-        for j = 1 : length(conditions)
-            if m == j
-                batch.Setup.conditions.onsets{j}{n}{m} = 9; % Nathalie discarded first 9 scans
-                batch.Setup.conditions.durations{j}{n}{m} = inf;
-            else
-                batch.Setup.conditions.onsets{j}{n}{m} = [];
-                batch.Setup.conditions.durations{j}{n}{m} = [];
-            end
+        for j = 1 : length(sessions_list)
+            batch.Setup.conditions.onsets{j}{sub_count+1}{ses_count} = [];
+            batch.Setup.conditions.durations{j}{sub_count+1}{ses_count} = [];
         end
-
+        batch.Setup.conditions.onsets{m}{sub_count+1}{ses_count} = 9; % Nathalie discarded first 9 scans
+        batch.Setup.conditions.durations{m}{sub_count+1}{ses_count} = inf;
     end
+
+    if ses_count > 0
+        sub_count = sub_count + 1;
+    end
+end
+
+if sub_count == 0
+    error(['No subjects found in ' path_subjects])
 end
 
 batch.filename = BATCHFILENAME;
 batch.Setup.isnew = 1;
-batch.Setup.nsubjects = length(sub_names);
+batch.Setup.nsubjects = sub_count;
 batch.Setup.RT = TR;
-batch.Setup.conditions.names = conditions;
+batch.Setup.conditions.names = sessions_list;
+BATCH.Setup.conditions.missingdata = 1;
 
 [conn_path, ~, ~] = fileparts(which('conn'));
 batch.Setup.rois.files{1} = [conn_path '/rois/atlas.nii'];
@@ -110,14 +116,14 @@ if setup_only
     return
 end
 
-% Preprocessing using 'VDM' and 'default_mni' options
+% Preprocessing using 'VDM' and 'default_mni' steps
 batch.Setup.preprocessing.steps = {'functional_label_as_original'; 
                                    'functional_vdm_create'; 
                                    'functional_vdm_apply'; 
                                    'functional_realign&unwarp';
                                    'functional_center';
-                                   'functional_slicetime';
-                                   'functional_art';
+                                 % 'functional_slicetime';
+                                   'functional_art';    
                                    'functional_segment&normalize_direct';
                                    'functional_label_as_mnispace';
                                    'structural_center';
@@ -129,13 +135,13 @@ batch.Setup.preprocessing.sliceorder = 'interleaved (Siemens)';
 batch.Setup.preprocessing.vdm_blip = blipdir;
 batch.Setup.done = 1;
 batch.Setup.overwrite = 'Yes';
-
-% Denoising                                    % Default options (uses White Matter+CSF+realignment+scrubbing+conditions as confound regressors); see conn_batch for additional options
-batch.Denoising.filter = [0.01, 0.1];                 % frequency filter (band-pass values, in Hz)
+                               
+% Default options (uses White Matter+CSF+realignment+scrubbing+conditions as confound regressors)
+batch.Denoising.filter = [0.01, 0.1]; % frequency filter (band-pass values, in Hz)
 batch.Denoising.done = 1;
 batch.Denoising.overwrite = 'Yes';
-
-% Analysis                                     % Default options (uses all ROIs in conn/rois/ as connectivity sources); see conn_batch for additional options
+                                
+% Default options (uses all ROIs in conn/rois/ as connectivity sources)
 batch.Analysis.done = 1;
 batch.Analysis.overwrite = 'Yes';
 
@@ -143,5 +149,3 @@ conn_batch(batch);
 conn
 conn('load', BATCHFILENAME);
 conn gui_analyses
-
-
